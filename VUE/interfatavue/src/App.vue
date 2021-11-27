1f26aeb40052fa33d73e9c0b0ab8c1b08cb9af12
IoanMarian08 <ioanmarianmarghioala8@gmail.com>
2021-11-27 17:37:52 +0200

    first commit

---
+++ VUE/interfatavue/src/App.vue
@@ -0,0 +1,65 @@
+<template id="body">
+  <div>
+    <img id="imgHome" alt="Vue logo" src="./assets/logoaplicatie.png" />
+    <HelloWorld msg="Bine ai venit pe E-lectron Components" />
+  
+    <div id="btnHome">
+      <button>Acceseaza aplicatia</button>
+      <button id="loginbtn">Login</button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button {
+  background-color: #41B783;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 40px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+button:hover {
+  filter: brightness(125%);
+}
+
+#imgHome {
+  width: 25%;
+}
+
+#btnHome {
+  display: flex;
+  flex-direction: column;
+  width: 10%;
+  margin: auto;
+}
+
+#loginbtn {
+  margin-top: 5px;
+}
+
+
+</style>
